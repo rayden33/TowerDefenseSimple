5d67585b219c75db6f2b8eb3c3600fa37dc962f0
Doniyorjon <rayden33@mail.ru>
2019-12-24 04:28:19 +0500

    monsters_movements

--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -4,18 +4,90 @@ using UnityEngine;
 
 public class MonsterScript : MonoBehaviour
 {
-    public int monsterHP = 10;
-    public int monsterDamage = 1;
-    public int deathGold = 1;
+    public float moveSpeed = 5.0f;
+
+    public static int monsterHP;
+
+    private char[,] mapTmp = GlobalData.mapMatrix;
+    private float mapMatrixX = GlobalData.mapMatrixX;
+    private float mapMatrixZ = GlobalData.mapMatrixZ;
+    private float diffX;
+    private float diffZ;
+    private Vector3 nextDir;
+    private int curX,curZ,nextX,nextZ;
     
+
+    private bool checkBorder(int x, int z)
+    {
+        return (x>=0 && x<mapMatrixX && z>=0 && z<mapMatrixZ);
+    }
+    private Vector3 getCoordinates(int x, int z)
+    {
+        return new Vector3(x - diffX, 0, z - diffZ);
+    }
+    private void findNextPointTarget()
+    {
+        for(int i=-1;i<=1;i++)
+        {
+            for(int j=-1;j<=1;j++)
+            {
+                if(Mathf.Abs(i+j)==1 && checkBorder(curX+i,curZ+j))
+                {
+                    if(mapTmp[curX+i,curZ+j]=='W'||mapTmp[curX+i,curZ+j]=='F')
+                    {
+                        mapTmp[curX,curZ] = 'D';
+                        nextX = curX+i;
+                        nextZ = curZ+j;
+                    }
+                }
+            }
+        }
+    }
+    private void damageMainTower()
+    {
+        GlobalData.playerHP = GlobalData.playerHP - GlobalData.monsterDamage;
+        selfDestroy();
+    }
+    private void selfDestroy()
+    {
+        if(transform.gameObject.name == GlobalData.monsterCountPerWave.ToString())
+        GlobalData.timeMode = 'W';
+        Destroy(transform.gameObject);
+    }
     void Start()
     {
-        
+        diffX = (mapMatrixX/2.0f);
+        diffZ = (mapMatrixZ/2.0f);
+        curX = GlobalData.startX;
+        curZ = GlobalData.startZ;
+        monsterHP = GlobalData.monsterHP;
+        findNextPointTarget();
     }
 
     
     void Update()
     {
-        
+        if(monsterHP <= 0)
+        {
+            selfDestroy();
+        }
+        if(Vector3.Distance(transform.position,getCoordinates(nextX,nextZ))<0.2f)
+        {
+            curX = nextX;
+            curZ = nextZ;
+            if(mapTmp[curX,curZ]=='F')
+            {
+                damageMainTower();
+            }
+            else
+            {
+                findNextPointTarget();
+            }
+        }
+        else
+        {
+            nextDir = getCoordinates(nextX,nextZ) - transform.localPosition;
+            transform.Translate(nextDir.normalized * Time.deltaTime*2);
+        }
     }
 }
